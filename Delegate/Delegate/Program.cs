ae6ba9e53b75684c1ba641bfbf2984d4c17450ad
Ashifa <84831054+Ashifa7@users.noreply.github.com>
2022-06-26 00:22:43 +0530

    Delegates

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Delegate
+{
+    public delegate void Calculate(int a, int b);
+    class Program
+    {
+        public static void Addition(int a, int b)
+        {
+            int c = a + b;
+            Console.WriteLine("Addition is: "+ c);
+        }
+        public static void Subtraction(int a, int b)
+        {
+            int c = a - b;
+            Console.WriteLine("Subtraction is: " + c);
+        }
+        public static void Multiplication(int a, int b)
+        {
+            int c = a * b;
+            Console.WriteLine("Multiplication is: " + c);
+        }
+        public static void Division(int a, int b)
+        {
+            int c = a / b;
+            Console.WriteLine("Division is: " + c);
+        }
+        static void Main(string[] args)
+        {
+            Calculate obj = new Calculate(Program.Addition);
+            obj(10,20);
+            obj = Subtraction;
+            obj(20,20);
+            obj = Multiplication;
+            obj(30,20);
+            obj = Division;
+            obj(40,20);
+
+            Console.ReadLine();
+        }
+    }
+}
