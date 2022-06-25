c02a61e869fc56414bb5d191867b53f9a5fb7271
Ashifa <84831054+Ashifa7@users.noreply.github.com>
2022-06-26 00:24:22 +0530

    Inheritance

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class Employee
+    {
+        public int salary = 35000;
+    }
+    public class Developer : Employee
+    {
+        public int bonus = 10000;
+    }
+    class Inheritance
+    {
+        static void Main(string[] args)
+        {
+            Developer D1 = new Developer();
+            Console.WriteLine("Salary: "+ D1.salary);
+            Console.WriteLine("Bonus: "+ D1.bonus);
+            Console.ReadKey();
+        }
+    }
+}
