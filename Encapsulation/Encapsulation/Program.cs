4024390377f570bc3c61a11a5fbaf46fae131104
Ashifa <84831054+Ashifa7@users.noreply.github.com>
2022-06-26 00:23:40 +0530

    Encapsulation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OopsConcepts
+{
+    public class Encapsulation
+    {
+        private string Name;                 // Name is a field, it must be private
+        private int Age;
+        private string City;                //wont be called in same class since no property defined
+
+        public string name                  // name is a property, used to access private fields or variables
+        {
+            get { return Name; }
+            set { Name = value; }           //Inshort, encapsulation property=gets private fields and sets it in value 
+        }
+
+        public int age
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+    }
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Encapsulation student = new Encapsulation();
+
+                student.name = "Ashifa";
+                student.age = 21;
+                student.City = "Ahmedabad";
+                Console.WriteLine("My Name is {0} and I am {1} years old", student.name, student.age);
+                Console.WriteLine("City = {0}", student.City);
+                Console.ReadKey();
+            }
+        }
+
+    }
+
