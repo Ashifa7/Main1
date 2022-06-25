bac62bd5cff12940867b1caeaf207399247b49a7
Ashifa <84831054+Ashifa7@users.noreply.github.com>
2022-06-26 00:18:37 +0530

    Abstraction - OOPs concept

---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Abstraction
+{
+    abstract class Animal
+    {
+        public abstract void animalsound();
+        public void Eat()
+        {
+            Console.WriteLine("Cat Eats Cat Food");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void animalsound()
+        {
+            Console.WriteLine("Cat says Meow");
+        }
+    }
+    
+    class Abstraction
+    {
+        static void Main(string[] args)
+        {
+            Cat c = new Cat();
+            c.animalsound();
+            c.Eat();
+        }
+    }
+        
+}
