9f1abca5f2ea3ff38037b408c1263ef97fa96124
Ashifa <84831054+Ashifa7@users.noreply.github.com>
2022-06-26 00:17:13 +0530

    Abstract Class Code

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AbstractClass
+{
+    abstract class Pen
+    {
+        public abstract void color();       //abstract method has no body
+        public void work()              //both simple and abstract methods can be declared in abstract class
+        {
+            Console.WriteLine("Writing");
+        }
+    }
+    class inkpen : Pen                  //implement abstract class
+    {
+        public override void color()        //override abstract method and now it has body
+        {
+            Console.WriteLine("Red");
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            inkpen obj = new inkpen();            //obj of implemented class
+            obj.color();
+            obj.work();                            //calling both methods
+            Console.ReadLine();
+        }
+    }
+}
