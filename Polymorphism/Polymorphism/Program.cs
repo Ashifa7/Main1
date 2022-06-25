d488d2bb2bb3f1cde1765013e2dc997d6940f221
Ashifa <84831054+Ashifa7@users.noreply.github.com>
2022-06-26 00:25:39 +0530

    Polymorphism

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace OopsConcepts
+{
+    public class SPBase                       //compile time polymorphism, method overloading,parameters change
+    {
+        public void AddNum(int a, int b)
+        {
+            Console.WriteLine("a + b = {0}", a + b);
+        }
+        public void AddNum(string c, int b, int a)                //number of parameter, order of parameters, and type of parameter different
+        {
+            Console.WriteLine("c + b + a = {0}", c + b + a);
+        }
+    }
+
+    public class DPBase                                      //Dynamic, RunTime, Method overriding with virtual keyword in base class method
+    {
+        public virtual void Info()
+        {
+            Console.WriteLine("This a Base Class");
+        }
+    }
+
+    public class DPderived : DPBase
+    {
+        public override void Info()
+        {
+            Console.WriteLine("This is a Derived Class");
+        }
+    }
+    class SP
+    {
+        static void Main(string[] args)
+        {
+            SPBase s = new SPBase();
+            s.AddNum(1, 10);
+            s.AddNum("ashifa", 1, 10);
+            Console.WriteLine("------------------------------------------");
+
+            DPBase db = new DPBase();
+            db.Info();
+            DPderived dd = new DPderived();
+            dd.Info();
+            Console.ReadLine();
+
+        }
+
+    }
+}
+
